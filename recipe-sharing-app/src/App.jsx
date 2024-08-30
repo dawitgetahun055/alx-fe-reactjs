a578e7e96bf5553b694e24d305b369f6ee114a49
Dawit Getahun <dawitgetahun055@gmail.com>
2024-08-30 19:42:21 +0300

    Creating Components for Favorites and Recommendations

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import RecipeList from "./components/RecipeList";
 
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
+import Recommendations from "./components/RecommendationsList";
 
 const App = () => (
   <Router>
     <div className="App">
       <h1>Recipe Sharing App</h1>
-      <SearchBar /> {/* Search bar at the top */}
+      <SearchBar />
       <AddRecipeForm />
+      <Recommendations />
+
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
