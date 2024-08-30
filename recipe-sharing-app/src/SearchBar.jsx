c3586e1df8ec7bd1edb6e9495fb2519bb2265f65
Dawit Getahun <dawitgetahun055@gmail.com>
2024-08-30 19:17:16 +0300

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/SearchBar.jsx
+++ recipe-sharing-app/src/SearchBar.jsx
@@ -2,12 +2,18 @@ import useRecipeStore from "./components/recipeStore";
 
 const SearchBar = () => {
   const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+    filteredRecipes();
+  };
 
   return (
     <input
       type="text"
       placeholder="Search recipes..."
-      onChange={(e) => setSearchTerm(e.target.value)}
+      onChange={handleSearchChange}
     />
   );
 };
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -6,12 +6,16 @@ const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
   return (
     <div>
-      {filteredRecipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      {filteredRecipes.length > 0 ? (
+        filteredRecipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      ) : (
+        <p>No recipes found</p>
+      )}
     </div>
   );
 };
