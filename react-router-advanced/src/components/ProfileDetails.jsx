d3fc055efd17856395b1711817d9a5eac3872cc9
Dawit Getahun <dawitgetahun055@gmail.com>
2024-09-02 00:07:14 +0300

    Updated

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,12 @@
+// src/components/ProfileDetails.jsx
+
+const ProfileDetails = () => {
+  return (
+    <div>
+      <h2>Profile Details</h2>
+      <p>Here are the details of the user's profile.</p>
+    </div>
+  );
+};
+
+export default ProfileDetails;
---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,13 @@
+// src/components/ProfileSettings.jsx
+import React from "react";
+
+const ProfileSettings = () => {
+  return (
+    <div>
+      <h2>Profile Settings</h2>
+      <p>Here you can update your profile settings.</p>
+    </div>
+  );
+};
+
+export default ProfileSettings;
