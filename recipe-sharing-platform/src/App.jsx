444e3c84d769de3c38d7becb81c9af3074f4780e
Dawit Getahun <dawitgetahun055@gmail.com>
2024-09-15 18:49:39 +0300

    Updated

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
 import Homepage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />}/>
-        <Route path="/recipe/:id" element={<RecipeDetail />}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
   );
